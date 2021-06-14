1d0353850a7da0fe5edbefeb9c98344aafa87551
Irina <balkarovairina@gmail.com>
2021-06-14 19:54:47 +0200

    add: ApplicationDbContext

---
+++ BookShop.Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace BookShop.Data
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+        
+    }
+}
