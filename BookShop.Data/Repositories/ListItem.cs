5622d5c27fdc2120c3f5e87c669020b560b838b5
Irina <balkarovairina@gmail.com>
2021-06-14 21:59:52 +0200

    add: ListItem which implements IBookListItemModel

---
+++ BookShop.Data/Repositories/ListItem.cs
@@ -0,0 +1,15 @@
+﻿using BookShop.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Data.Repositories
+{
+    public class ListItem : IBookListItemModel
+    {
+        public int Id { get; set; }
+        public string BookTitle { get; set; }
+        public string AuthorName { get; set; }
+        public string Genre { get; set; }
+    }
+}
