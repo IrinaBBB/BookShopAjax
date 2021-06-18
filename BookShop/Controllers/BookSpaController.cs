9fed1a3aa279ba73a9dd5b26ee9001f034c80040
Irina <balkarovairina@gmail.com>
2021-06-18 09:31:37 +0200

    add: Data Tables for BookSPA View

--- BookShop/Controllers/BookSpaController.cs
+++ BookShop/Controllers/BookSpaController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookShop.Shared.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookShop.Controllers
 {
     public class BookSpaController : Controller
     {
+        private readonly IBooksRepository _repository;
+
+        public BookSpaController(IBooksRepository repository)
+        {
+            _repository = repository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var books = _repository.GetBookList();
+            return View(books);
         }
     }
 }
